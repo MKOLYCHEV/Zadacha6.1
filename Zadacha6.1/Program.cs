39280b501b0808949d8eadb058ef1f4036ff2605
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-12-04 13:18:41 +0300

    Добавьте файлы проекта.

---
+++ Zadacha6.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение (слова разделять пробелами, знаки препинания не использовать).");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string maxLength = string.Empty;
+            for(int i=0;i<stringArray.Length;i++)
+            {
+                if (stringArray[i].Length > maxLength.Length)
+                    maxLength = stringArray[i];
+            }
+            Console.WriteLine("Наибольшее слово в веденном предложении \"{0}\".",maxLength);
+            Console.ReadKey();
+        }
+    }
+}
